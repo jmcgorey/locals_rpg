5599d45ffa812ab7bfe6535fba703489aea29af2
John McGorey <j.t.mcgorey@gmail.com>
2025-04-24 23:07:02 -0500

    polish pass

--- scripts/interaction/interaction_area.gd
+++ scripts/interaction/interaction_area.gd
@@ -1,6 +1,9 @@
 extends Area2D
 class_name InteractionArea
 
+var interact_text_scene = preload("res://scenes/interaction/interact_text.tscn")
+var interact_text_instance: Node2D
+
 # If true, the area is able to receive interactions
 var _is_active = true
 
@@ -24,12 +27,25 @@ func register():
 func deregister():
 	InteractionManager.unregister_area(self)
 	
+func show_interact_text() -> void:
+	interact_text_instance = interact_text_scene.instantiate()
+	add_child(interact_text_instance)
+	interact_text_instance.global_position = self.global_position
+	interact_text_instance.global_position.y -= 15
+
+
+func hide_interact_text() -> void:
+	if interact_text_instance != null:
+		interact_text_instance.queue_free()
+	
 # Automatically register the area when the player enters it
 func _on_body_entered(_body: Node2D) -> void:
 	if _body is Player and _is_active:
 		register()
+		show_interact_text()
 		
 # Automatically deregister the area when the player leaves it
 func _on_body_exited(_body: Node2D) -> void:
 	if _body is Player:
 		deregister()
+		hide_interact_text()
